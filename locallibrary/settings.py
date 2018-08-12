223f55d4426ed8442ece54b0b86d66f8441be708
Oxfos <rinaldo_bertossa@hotmail.com>
2018-08-12 13:14:04 +0200

    changed file name, added model graphs

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog.apps.CatalogConfig',
+    'django_extensions',
+    'graphviz',
 ]
 
 MIDDLEWARE = [
